d41101b86f135d96cfb1c19b180facabf8ac02fc
chuahys <chuahys02@gmail.com>
2025-01-15 16:58:36 +0800

    add comments for main, piece, color, type, factory

--- PieceColor.java
+++ PieceColor.java
@@ -1,6 +1,13 @@
+/**
+ * Represents the color of a piece in the game. (Blue or Red)
+ * @author Chuah Yun Shan
+ */
 public enum PieceColor {
     BLUE, RED;
 
+    /**
+     * Return "Blue" if the color is BLUE, "Red" if the color is RED.
+     */
     @Override
     public String toString() {
         return this == BLUE ? "Blue" : "Red";
--- PieceFactory.java
+++ PieceFactory.java
@@ -1,6 +1,11 @@
+/**
+ * Factory design pattern for creating instances of pieces in the game.
+ * @author Tan Yun Xuan
+ */
 public class PieceFactory {
     // Create a new piece based on the given type, color, and position (row, col)
     public static Piece createPiece(PieceType type, PieceColor color, int row, int col) {
+        // Determine the type of piece
         switch (type) {
             case RAM:
                 return new Ram(color, row, col);
